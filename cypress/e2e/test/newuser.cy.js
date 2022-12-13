37433f636dd109257118512a08493cb532046590
Chinedu-Nwolisa <smartnwolisa39@gmail.com>
2022-12-13 21:45:44 +0200

    ned

--- cypress/e2e/test/newuser.cy.js
+++ cypress/e2e/test/newuser.cy.js
@@ -11,7 +11,7 @@ console.log()
 
 
 
-describe("Login functionali", () => {
+describe("Login functionality", () => {
     beforeEach(() => {
       cy.visit(locators.URL);
       cy.get(locators.homepageSignin).click();
