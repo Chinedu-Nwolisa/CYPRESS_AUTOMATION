e72f7f15f78311dc8d610cfdb935caf62efc0a9a
Chinedu-Nwolisa <smartnwolisa39@gmail.com>
2022-12-31 17:13:46 +0200

    another trial

--- cypress/e2e/test/newuser.cy.js
+++ cypress/e2e/test/newuser.cy.js
@@ -11,7 +11,7 @@ console.log()
 
 
 
-describe("Login_function", () => {
+describe("Login_functionality", () => {
     beforeEach(() => {
       cy.visit(locators.URL);
       cy.get(locators.homepageSignin).click();
@@ -19,7 +19,7 @@ describe("Login_function", () => {
   
   
     it("User should be able to create an account", () => {
-      cy.get(locators.NewUseremail)type(email)
+      cy.get(locators.NewUseremail).type(email)
       cy.get(locators.RegisterBtn).click()
       cy.get(locators.SelectGender).click()
       cy.get(locators.firstNameField).type(firstName)
